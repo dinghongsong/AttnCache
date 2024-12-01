91b4a062c8d5137298af4a9bcde6f5bfc7625760
dinghongsong <dinghongsong21@gmail.com>
2024-12-01 12:56:17 -0800

    update download data

--- download_dataset.sh
+++ download_dataset.sh
@@ -1,2 +1,2 @@
 wget -P SentEval/data/downstream/ --no-check-certificate https://huggingface.co/datasets/princeton-nlp/datasets-for-simcse/resolve/main/senteval.tar
-tar xvf senteval.tar
+tar xvf SentEval/data/downstream/senteval.tar -C SentEval/data/downstream/
